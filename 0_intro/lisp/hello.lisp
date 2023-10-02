a61b035bb2c7133e27d7c347a37dc162241a0795
chechyotka <romanchechyotkin@gmail.com>
2023-10-02 18:17:55 +0300

    intro

--- 0_intro/lisp/hello.lisp
+++ 0_intro/lisp/hello.lisp
@@ -1,2 +1,20 @@
+(defun fib (n)
+    "Return the nth Fibonacci number"
+    (if (< n 2)
+        n
+        (+ (fib (- n 1))
+           (fib (- n 2))
+        )
+    ))
+
 (defun hello ()
-    (format t "Hello World~%"))
+    (format t "Hello World~%")
+)
+
+(defun many (n) 
+    (multiple-value-list (values n (* n n) (* n n n)))
+)
+
+(defun get-idx (n list)
+    (nth n list)
+)
